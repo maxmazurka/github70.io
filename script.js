4e42cc9081fd97967eb668f544609821517487f3
maxmazurka <maks_lord_2020@mail.ru>
2024-10-29 01:17:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+$(document).ready(function () {
+  $(".carousel").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
